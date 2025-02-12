662a9ae1858a0c031103d103ca20021483caab51
Suhani_Gupta <suhu786.gupta@gmail.com>
2025-02-12 23:36:33 +0530

    last commit

--- script.js
+++ script.js
@@ -46,3 +46,8 @@ async function checkWeather(city) {
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value)
 }) 
+searchBox.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        checkWeather(searchBox.value);
+    }
+})
