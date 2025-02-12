72bfa326ed082c1534257598b3a1a3bf2e7d5737
Suhani_Gupta <suhu786.gupta@gmail.com>
2025-02-12 23:34:14 +0530

    fifth commit

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
 const apiKey="f539ab4cf7246a1239abd826615c8746";
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-    const response= await fetch(apiUrl + `&appId=${apiKey}`);
-    var data= await response.json();
-    console.log(data);
+const searchBox= document.querySelector(".search input");
+const searchBtn= document.querySelector(".search button");
+const weatherIcon= document.querySelector(".weather-icon")
+
+async function checkWeather(city) {
+    const response= await fetch(apiUrl + city +  `&appId=${apiKey}`);
+
+    if(response.status==404){
+        document.querySelector(".error").style.display= "block";
+        document.querySelector(".weather").style.display= "none";
+    }
+    else{
+        var data= await response.json();
+
 
     document.querySelector(".city").innerHTML= data.name;
     document.querySelector(".temp").innerHTML= Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML= data.main.humidity + "%"; 
     document.querySelector(".wind").innerHTML= data.wind.speed + " km/h";   
+
+
+
+    if(data.weather[0].main=="Clouds"){
+        weatherIcon.src= "images/clouds.png";
+    }
+    else if(data.weather[0].main=="Clear"){
+        weatherIcon.src= "images/clear.png";
+    }
+    else if(data.weather[0].main=="Rain"){
+        weatherIcon.src= "images/rain.png";
+    }
+    else if(data.weather[0].main=="Drizzle"){
+        weatherIcon.src= "images/drizzle.png";
+    }
+    else if(data.weather[0].main=="Mist"){
+        weatherIcon.src= "images/mist.png";
+    }
+    document.querySelector(".weather").style.display= "block";
+    document.querySelector(".error").style.display= "none";
+    }
 }
- checkWeather();
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value)
+}) 
